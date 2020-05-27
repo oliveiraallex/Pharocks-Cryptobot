abd74316b4ef9f69062434c64a4d53b3c31bca0d
Allex Oliveira <allex.oliveira@yahoo.com.br>
2020-05-28 00:23:50 +0200

    Pharo Image creator config file

---
+++ imageConf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+export PROJECT_NAME='Pharocks'
+
+PHARO[0]=70
+PHARO[1]=80
+PHARO[2]=90
+
+export ARCH_32=true
+export ARCH_64=true
+
+export PHARO_SCRIPT="Iceberg enableMetacelloIntegration: false.
+
+Metacello new
+  baseline: 'Pharocks';
+  repository: 'github://oliveiraallex/Pharocks-Cryptobot';
+  load.
+
+Smalltalk saveSession.
+
+Iceberg enableMetacelloIntegration: true."
